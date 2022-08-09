e44d324202dbf0b333a74bbec163d0862c7b6058
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-09 13:17:13 -0300

    using criteria API to create typed queries

--- src/main/java/com/nasa/nafood/infra/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/nasa/nafood/infra/repository/RestaurantRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.nasa.nafood.infra.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -23,39 +26,70 @@ public class RestaurantRepositoryImpl implements CustomRestaurantRepository {
 	
 	@Override
 	public List<Restaurant> findByNameBetweenFee(String name, BigDecimal initialFee, BigDecimal finalFee) {
-//		var jpql = "FROM Restaurant "
-//				+ "WHERE name LIKE :name "
-//				+ "AND fee BETWEEN :initialFee AND :finalFee";
 		
-		Map<String, Object> parameters = new HashMap<String, Object>();
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
 		
-		var jpql = new StringBuilder();
+		CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
 		
-		jpql.append("FROM Restaurant where 0=0 ");
+		Root<Restaurant> root = criteria.from(Restaurant.class); // "FROM Restaurant"
+		
+		List<Predicate> predicates = new ArrayList<>();
 		
 		if(StringUtils.hasLength(name)) {
-			jpql.append("and name LIKE :name ");
-			parameters.put("name", "%" + name + "%");
+			predicates.add(builder.like(root.get("name"), "%" + name + "%"));
 		}
 		
 		if(initialFee != null) {
-			jpql.append("and fee >= :initialFee ");
-			parameters.put("initialFee", initialFee);
+			predicates.add(builder.greaterThanOrEqualTo(root.get("fee"), initialFee));
 		}
 		
 		if(finalFee != null) {
-			jpql.append("and fee <= :finalFee");
-			parameters.put("finalFee", finalFee);
+			predicates.add(builder.lessThanOrEqualTo(root.get("fee"), finalFee));
 		}
 		
-		TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
 		
+		criteria.where(predicates.toArray(new Predicate[0]));
 		
-		
-		parameters.forEach((key, value) -> {
-			query.setParameter(key, value);
-		});
+		TypedQuery<Restaurant> query = entityManager.createQuery(criteria);
 		
 		return query.getResultList();
 	}
+	
+//	@Override
+//	public List<Restaurant> findByNameBetweenFee(String name, BigDecimal initialFee, BigDecimal finalFee) {
+////		var jpql = "FROM Restaurant "
+////				+ "WHERE name LIKE :name "
+////				+ "AND fee BETWEEN :initialFee AND :finalFee";
+//		
+//		Map<String, Object> parameters = new HashMap<String, Object>();
+//		
+//		var jpql = new StringBuilder();
+//		
+//		jpql.append("FROM Restaurant where 0=0 ");
+//		
+//		if(StringUtils.hasLength(name)) {
+//			jpql.append("and name LIKE :name ");
+//			parameters.put("name", "%" + name + "%");
+//		}
+//		
+//		if(initialFee != null) {
+//			jpql.append("and fee >= :initialFee ");
+//			parameters.put("initialFee", initialFee);
+//		}
+//		
+//		if(finalFee != null) {
+//			jpql.append("and fee <= :finalFee");
+//			parameters.put("finalFee", finalFee);
+//		}
+//		
+//		TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
+//		
+//		
+//		
+//		parameters.forEach((key, value) -> {
+//			query.setParameter(key, value);
+//		});
+//		
+//		return query.getResultList();
+//	}
 }
