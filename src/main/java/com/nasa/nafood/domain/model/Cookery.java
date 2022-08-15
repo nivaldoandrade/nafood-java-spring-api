955f8c6ea7483e01cbe8d054755db966e2af446c
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-15 17:44:06 -0300

    feat: mapping bidirectional relationship with @OneToMany

--- src/main/java/com/nasa/nafood/domain/model/Cookery.java
+++ src/main/java/com/nasa/nafood/domain/model/Cookery.java
@@ -1,10 +1,15 @@
 package com.nasa.nafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.EqualsAndHashCode;
@@ -24,4 +29,8 @@ public class Cookery {
 	private Long id;
 	
 	private String name;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cookery")
+	private List<Restaurant> restaurants = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,10 @@
 INSERT INTO cookery(name) VALUES('Brasileira');
 INSERT INTO cookery(name) VALUES('Italiana');
+INSERT INTO cookery(name) VALUES('Mineira');
 
 INSERT INTO restaurant(name, fee, cookery_id) VALUES('Outback', '111.90', 1);
 INSERT INTO restaurant(name, fee, cookery_id) VALUES('Madero', '90.5', 2);
-INSERT INTO restaurant(name, fee, cookery_id) VALUES('Bar do Peixe', '35', 1);
+INSERT INTO restaurant(name, fee, cookery_id) VALUES('Bar do Peixe', '35', 3);
 INSERT INTO restaurant(name, fee, cookery_id) VALUES('Andrades Restaurant', 0,  2);
 INSERT INTO restaurant(name, fee, cookery_id) VALUES('Valderi', 0, 1);
 
@@ -11,7 +12,7 @@ INSERT INTO payment(description) VALUES('Cash');
 INSERT INTO payment(description) VALUES('Debit cards');
 INSERT INTO payment(description) VALUES('Credit cards');
 
-INSERT INTO  permission(description) VALUES('Admin');
+INSERT INTO permission(description) VALUES('Admin');
 INSERT INTO permission(description) VALUES('User');
 
 INSERT INTO state(name) VALUES('São Paulo');
