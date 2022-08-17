d0736fd108da15f970f5dbe054341f63239c6fe0
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-17 10:13:51 -0300

    feat: create product model and @ManyToOne relationship of product with restaurant

---
+++ src/main/java/com/nasa/nafood/domain/model/Product.java
@@ -0,0 +1,36 @@
+package com.nasa.nafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+@Entity
+public class Product {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String description;
+	
+	private BigDecimal price;
+	
+	private boolean active;
+	
+	@ManyToOne
+	private Restaurant restaurant;
+}
