6fea02e85d21006be12078e6d418db55c12acadf
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-23 17:36:30 -0300

    refactor: adding test data to Order and OrderItem table with Flyway Callback

--- src/main/resources/db/datatest/afterMigrate.sql
+++ src/main/resources/db/datatest/afterMigrate.sql
@@ -3,12 +3,16 @@ set foreign_key_checks=0;
 delete from city;
 delete from state;
 delete from cookery;
+delete from order_;
+delete from order_item;
 
 set foreign_key_checks=1;
 
 ALTER TABLE city auto_increment=1;
 ALTER TABLE state auto_increment=1;
 ALTER TABLE cookery auto_increment=1;
+ALTER TABLE order_ auto_increment=1;
+ALTER TABLE order_item auto_increment=1;
 
 INSERT INTO cookery(name) VALUES('Brasileira');
 INSERT INTO cookery(name) VALUES('Italiana');
@@ -20,3 +24,9 @@ INSERT INTO state(name) VALUES('Piauí');
 
 INSERT INTO city(name, state_id) VALUES('São Caetano do Sul', 1);
 INSERT INTO city(name, state_id) VALUES('Niterói', 2);
+
+INSERT INTO order_(sub_total, fee, total_amount, status, created_at)
+VALUES(100, 35, 135, 'CRIADO', utc_timestamp);
+
+INSERT INTO order_item(order_id, quantity, unit_price, total_price, observation) 
+VALUES(1, 4, 12.5, 50, 'Test observation');
