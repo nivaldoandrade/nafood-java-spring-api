3c5a12f01cfb1ed9358d223db24a000775d73e0c
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-27 18:10:43 -0300

    adding test data to User and modifying test data from table Order with Flyway Callback

--- src/main/resources/db/datatest/afterMigrate.sql
+++ src/main/resources/db/datatest/afterMigrate.sql
@@ -7,6 +7,7 @@ delete from order_;
 delete from order_item;
 delete from payment;
 delete from restaurant;
+delete from user;
 
 set foreign_key_checks=1;
 
@@ -18,6 +19,7 @@ ALTER TABLE order_item auto_increment=1;
 ALTER TABLE order_item auto_increment=1;
 ALTER TABLE payment auto_increment=1;
 ALTER TABLE restaurant auto_increment=1;
+ALTER TABLE user auto_increment=1;
 
 INSERT INTO cookery(name) VALUES('Brasileira');
 INSERT INTO cookery(name) VALUES('Italiana');
@@ -37,12 +39,16 @@ INSERT INTO payment(description) VALUES('Credit cards');
 INSERT INTO restaurant(name, fee, active, opened, created_at, updated_at) VALUES('Andrades Rest', 50.7, 1, 1, utc_timestamp, utc_timestamp);
 INSERT INTO restaurant(name, fee, active, opened, created_at, updated_at) VALUES('Outback', 123.7, 1, 0, utc_timestamp, utc_timestamp);
 
-INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, created_at)
-VALUES(100, 35, 135, 'CRIADO', 1, 1, utc_timestamp);
-INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, created_at)
-VALUES(248, 34, 121, 'CRIADO', 1, 2, utc_timestamp);
+INSERT INTO user(name, email, password) VALUES('Jonh Doe', 'johndoe@email.com', '123456');
+INSERT INTO user(name, email, password) VALUES('Jonh Doe 2 ', 'johndoe2@email.com', '654321');
+
+INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, created_at)
+VALUES(100, 35, 135, 'CRIADO', 1, 1, 2, utc_timestamp);
+INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, created_at)
+VALUES(248, 34, 121, 'CRIADO', 1, 2, 1, utc_timestamp);
 
 INSERT INTO order_item(order_id, quantity, unit_price, total_price, observation) 
 VALUES(1, 4, 12.5, 50, 'Test observation');
 INSERT INTO order_item(order_id, quantity, unit_price, total_price, observation) 
 VALUES(2, 4, 12.5, 50, 'Test observation 2');
+
