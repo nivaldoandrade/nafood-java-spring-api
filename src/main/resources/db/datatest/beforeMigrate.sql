e0ec3fc496f0d259aa88682141a7c68f9b5fabbf
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-29 16:50:02 -0300

    feat: create BeforeMigrate file to clean all tables before running migration

---
+++ src/main/resources/db/datatest/beforeMigrate.sql
@@ -0,0 +1,22 @@
+set foreign_key_checks=0;
+
+delete from city;
+delete from state;
+delete from cookery;
+delete from order_;
+delete from order_item;
+delete from payment;
+delete from restaurant;
+delete from user;
+
+set foreign_key_checks=1;
+
+ALTER TABLE city auto_increment=1;
+ALTER TABLE state auto_increment=1;
+ALTER TABLE cookery auto_increment=1;
+ALTER TABLE order_ auto_increment=1;
+ALTER TABLE order_item auto_increment=1;
+ALTER TABLE order_item auto_increment=1;
+ALTER TABLE payment auto_increment=1;
+ALTER TABLE restaurant auto_increment=1;
+ALTER TABLE user auto_increment=1;
