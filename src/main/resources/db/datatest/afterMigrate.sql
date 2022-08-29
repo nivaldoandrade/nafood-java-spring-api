e616a795c4e53b14b73983eef521d295df031f40
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-29 16:47:50 -0300

    modifying test data from table Order with Flyway Callback

--- src/main/resources/db/datatest/afterMigrate.sql
+++ src/main/resources/db/datatest/afterMigrate.sql
@@ -1,26 +1,3 @@
-set foreign_key_checks=0;
-
-delete from city;
-delete from state;
-delete from cookery;
-delete from order_;
-delete from order_item;
-delete from payment;
-delete from restaurant;
-delete from user;
-
-set foreign_key_checks=1;
-
-ALTER TABLE city auto_increment=1;
-ALTER TABLE state auto_increment=1;
-ALTER TABLE cookery auto_increment=1;
-ALTER TABLE order_ auto_increment=1;
-ALTER TABLE order_item auto_increment=1;
-ALTER TABLE order_item auto_increment=1;
-ALTER TABLE payment auto_increment=1;
-ALTER TABLE restaurant auto_increment=1;
-ALTER TABLE user auto_increment=1;
-
 INSERT INTO cookery(name) VALUES('Brasileira');
 INSERT INTO cookery(name) VALUES('Italiana');
 INSERT INTO cookery(name) VALUES('Mineira');
@@ -45,10 +22,10 @@ VALUES('Outback', 123.7, 1, 0, 'Alameda Terracota', '545 - 1079', 'Cerâmica', 1
 INSERT INTO user(name, email, password) VALUES('Jonh Doe', 'johndoe@email.com', '123456');
 INSERT INTO user(name, email, password) VALUES('Jonh Doe 2 ', 'johndoe2@email.com', '654321');
 
-INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, created_at)
-VALUES(100, 35, 135, 'CRIADO', 1, 1, 2, utc_timestamp);
-INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, created_at)
-VALUES(248, 34, 121, 'CRIADO', 1, 2, 1, utc_timestamp);
+INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, address_address, address_address2, address_number, address_district, city_id, created_at)
+VALUES(100, 35, 135, 'CRIADO', 1, 1, 2, 'Rua Alagoas', 'Bar', '664', 'Centro', 1, utc_timestamp);
+INSERT INTO order_(sub_total, fee, total_amount, status, payment_id, restaurant_id, user_id, address_address, address_number, address_district, city_id, created_at)
+VALUES(248, 34, 121, 'CRIADO', 1, 2, 1, 'Alameda Terracota', '545 - 1079', 'Cerâmica', 1, utc_timestamp);
 
 INSERT INTO order_item(order_id, quantity, unit_price, total_price, observation) 
 VALUES(1, 4, 12.5, 50, 'Test observation');
