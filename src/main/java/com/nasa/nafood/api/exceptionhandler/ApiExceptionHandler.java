58eb96d0773dedfcc90929dc8e8069c948417fbf
nivaldoandrade <nivaldoandradef@gmail.com>
2022-09-24 19:48:20 -0300

    feat: customizing ResponseEntityExceptionHandler's default response body

--- src/main/java/com/nasa/nafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/nasa/nafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,10 +2,12 @@ package com.nasa.nafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.nasa.nafood.domain.exception.EntityBadRequestException;
@@ -16,14 +18,18 @@ import com.nasa.nafood.domain.exception.EntityNotFoundException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<?> handleEntityNotFoundExpection(EntityNotFoundException e) {
-		ApiError error = ApiError.builder()
-				.timestamp(LocalDateTime.now())
-				.message(e.getMessage()).build();
+	public ResponseEntity<?> handleEntityNotFoundExpection(EntityNotFoundException ex) {
 		
+		return handleExceptionInternal(ex, ex.getMessage(), null, HttpStatus.NOT_FOUND, null);
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(error);
+	
+//		ApiError error = ApiError.builder()
+//				.timestamp(LocalDateTime.now())
+//				.message(e.getMessage()).build();
+//		
+//		
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//				.body(error);
 	}
 	
 	@ExceptionHandler(EntityBadRequestException.class)
@@ -45,5 +51,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(error);
+	}
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+
+		if(body == null) {
+			body = ApiError.builder()
+					.timestamp(LocalDateTime.now())
+					.message(status.getReasonPhrase())
+					.build();
+		} else {
+			body = ApiError.builder()
+					.timestamp(LocalDateTime.now())
+					.message((String) body)
+					.build();
+		}
+		
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
